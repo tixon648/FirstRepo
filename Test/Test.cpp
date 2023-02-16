256e88fcdd57c276802a40ce0101efd8658aee4f
tixon648 <tlucojanov@gmail.com>
2023-02-16 15:51:44 +0300

    конвертирования единиц измерения с использованием цикла for и использования if

---
+++ Test/Test.cpp
@@ -0,0 +1,26 @@
+﻿// Test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+// Создание таблицы конвертирования единиц измерения с использованием цикла for и использования if
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");// Подключение русского языка;
+    double SeaMile,KMeters;
+    int counter = 0;
+    for (KMeters = 1.0; KMeters <= 50.0; KMeters++) {
+        SeaMile = KMeters / 1.852;
+        cout << "Километров:  " << KMeters <<"\t\t"<< "Морских миль: " << SeaMile << "\n";
+        counter++;
+        if (counter == 5) {
+            cout << "\n";
+            counter = 0;
+        }
+          
+    }
+    return 0;
+}
+
